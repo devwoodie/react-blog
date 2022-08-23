fb1117b52c58d3bc96b80ba0e09b98938e507052
ehddn453 <ehddl453@naver.com>
2022-08-23 23:14:50 +0900

    feat : use JSX, useState, event handler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+/* eslint-disable */
+
 import './Css/App.css';
 import './Css/Reset.css';
 import {useState} from "react";
 
 function App() {
-    let [title, setTitle] = useState(['Title1', 'Title2', 'Title3']);
+    let [mainTitle, setMainTitle] = useState(['Q Blog Title 1', 'F Blog Title 2', 'K Blog Title 3']);
+    let [like, setLike] = useState(0);
+
     return (
         <div className="App">
             <h1 className="logo">ReactBlog</h1>
+            <button type="button" onClick={() => {
+                let sortCopy = [... mainTitle];
+                sortCopy.sort();
+                setMainTitle(sortCopy);
+            }}>Alphabet Sort()</button>
             <div className="list">
-                <p className="title">{ title[0] }</p>
+                <p className="title">{ mainTitle[0] } <span className="like-btn" onClick={() => {setLike(like+1)}}>👍</span> { like }</p>
+                <span className="text">2월 17일 발행</span>
+                <button type="button" onClick={() => {
+                    let copy = [... mainTitle];
+                    copy[0] = 'T Change Title 1';
+                    setMainTitle(copy);
+                }}>Change Title</button>
+            </div>
+            <div className="list">
+                <p className="title">{ mainTitle[1] }</p>
                 <span className="text">2월 17일 발행</span>
             </div>
             <div className="list">
-                <p className="title">{ title[1] }</p>
-                <span className="text">2월 17일 발행</span>
-            </div>
-            <div className="list">
-                <p className="title">{ title[2] }</p>
+                <p className="title">{ mainTitle[2] }</p>
                 <span className="text">2월 17일 발행</span>
             </div>
         </div>
